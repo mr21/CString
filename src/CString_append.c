9bc6af58e78f61efbdd7f6022d93af547991b294
Mr21 <thomastortorini@gmail.com>
2012-07-27 23:46:54 +0200

    append: un size_t en moins

--- src/CString_append.c
+++ src/CString_append.c
@@ -1,10 +1,8 @@
 #include	<string.h>
 #include	"CString.h"
 
-static CString*	append(CString* s, char const* chr, size_t sz_chr)
+static CString*	append(CString* s, char const* chr, size_t sz)
 {
-  size_t	sz = s->size + sz_chr;
-
   if (sz >= s->tab_size && CString_resize(s, sz))
     return NULL;
   (!s->size ? strcpy : strcat)(s->str, chr);
@@ -12,12 +10,12 @@ static CString*	append(CString* s, char const* chr, size_t sz_chr)
   return s;
 }
 
-CString*	CString_append_chr(CString* str, char const* cpy)
+CString*	CString_append_chr(CString* s, char const* cpy)
 {
-  return append(str, cpy, strlen(cpy));
+  return append(s, cpy, s->size + strlen(cpy));
 }
 
-CString*	CString_append_str(CString* str, CString const* cpy)
+CString*	CString_append_str(CString* s, CString const* cpy)
 {
-  return append(str, cpy->str, cpy->size);
+  return append(s, cpy->str, s->size + cpy->size);
 }
